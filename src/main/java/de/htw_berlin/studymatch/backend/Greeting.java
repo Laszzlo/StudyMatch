2787ab10228f9c98f199d03f09e8cc5c2fe00936
Laszlo Imanuel <laszlo.imanuel@campus-tu.berlin.de>
2025-10-13 15:43:23 +0200

    GET ROUTE erstellt

---
+++ src/main/java/de/htw_berlin/studymatch/backend/Greeting.java
@@ -0,0 +1,5 @@
+package de.htw_berlin.studymatch.backend;
+
+public record Greeting(long id, String content) {
+
+}
---
+++ src/main/java/de/htw_berlin/studymatch/backend/model/User.java
@@ -0,0 +1,8 @@
+package de.htw_berlin.studymatch.backend.model;
+
+public class User {
+    private Long uid;
+    private String vorname;
+    private String nachname;
+    private String email;
+}
