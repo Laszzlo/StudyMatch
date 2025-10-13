468ea1e79dd7052960f0ae80d1061b9b3ee04139
Laszlo Imanuel <laszlo.imanuel@campus-tu.berlin.de>
2025-10-13 14:08:34 +0200

    Projekt erstellt

---
+++ src/test/java/de/htw_berlin/studymatch/StudyMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.studymatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
