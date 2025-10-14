42b3f59b3b987394591c5f1e84f4bbeb5ee0aace
Laszlo Imanuel <laszlo.imanuel@campus-tu.berlin.de>
2025-10-14 07:48:14 +0200

    M1

--- src/main/java/de/htw_berlin/studymatch/backend/controller/UserController.java
+++ src/main/java/de/htw_berlin/studymatch/backend/controller/UserController.java
@@ -1,19 +1,28 @@
 package de.htw_berlin.studymatch.backend.controller;
 import de.htw_berlin.studymatch.backend.Greeting;
+import de.htw_berlin.studymatch.backend.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class UserController {
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    @GetMapping("/user")
+    public List<User> getUser() {
+        return List.of(User.builder()
+                .vorname("Mia")
+                .nachname("Mitrovic")
+                .email("mia@mitrovic.com")
+                .build(),
+                User.builder()
+                .vorname("Laszlo")
+                .nachname("Imanuel")
+                .email("laszlo@imanuel.com")
+                .build());
     }
 
 }
--- src/main/java/de/htw_berlin/studymatch/backend/model/User.java
+++ src/main/java/de/htw_berlin/studymatch/backend/model/User.java
@@ -1,6 +1,15 @@
 package de.htw_berlin.studymatch.backend.model;
 
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+    @Setter(AccessLevel.NONE)
     private Long uid;
     private String vorname;
     private String nachname;
