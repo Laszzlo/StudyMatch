f382b86ca4cd39b00715747027a78d7a06d3410d
Laszlo Imanuel <laszlo.imanuel@campus-tu.berlin.de>
2025-11-17 01:37:31 +0100

    @EnableWebSecurity hinzugefügt

--- src/main/java/de/htw_berlin/studymatch/backend/config/SecurityConfig.java
+++ src/main/java/de/htw_berlin/studymatch/backend/config/SecurityConfig.java
@@ -3,15 +3,16 @@ package de.htw_berlin.studymatch.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         http
                 .csrf(csrf -> csrf.disable()) // Für Postman / REST
                 .authorizeHttpRequests(auth -> auth
